5a7ee84ee1783f222c2e9920d5bc68decb7f0326
Sangeetha <sangeethas12122000@gmail.com>
2022-08-27 19:14:54 +0530

    profile icon added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
+import PersonIcon from '@mui/icons-material/Person';
+
+import IconButton from '@mui/material/IconButton';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h2>I am header</h2>
+      <IconButton>
+      <PersonIcon fontSize='large' className='header__icon'/>
+      </IconButton>
     </div>
   );
 }
