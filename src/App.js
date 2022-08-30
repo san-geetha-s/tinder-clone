60c3b49643b73fd2fd24ed256073dea7e6d6b70f
Sangeetha <sangeethas12122000@gmail.com>
2022-08-30 14:11:17 +0530

    tindercards added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@
 import './App.css';
 
 import Header from './Header';
-
+import TinderCards from './TinderCards';
 
 function App() {
   return (
     <div className="app">
      <Header/>
+     <TinderCards/>
       
 
     </div>
