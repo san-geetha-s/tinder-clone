4c0a0e8d0291eee5d474ada1c23d3963c3553d3b
Sangeetha <sangeethas12122000@gmail.com>
2022-09-01 19:03:56 +0530

    frontend completed

--- src/TinderCards.js
+++ src/TinderCards.js
@@ -1,31 +1,27 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './TinderCards.css'
 import TinderCard from "react-tinder-card"
+import axios from './axios'
+
 
 function TinderCards() {
   const [people,setPeople] =useState([
+    
+     
+  ]);
 
-    {
-      name:'Elon Musk',
-      url:"https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Elon_Musk_Royal_Society_%28crop2%29.jpg/1200px-Elon_Musk_Royal_Society_%28crop2%29.jpg",
+  useEffect(()=>{
+    async function fetchData(){
+      const req=await axios.get("/tinder/cards")
 
-    },
-    {
-      name:"Christiano Ronaldo",
-      url:"https://i.ds.at/-TEqAQ/rs:fill:750:0/plain/2022/08/17/ronaldo.jpg",
-      
-
-    },{
-      name:"Steve Harvey",
-      url:"https://images.mubicdn.net/images/cast_member/40212/cache-246020-1502585990/image-w856.jpg?size=800x",
-
-    },
-    {
-      name:"Larry Page",
-      url:"https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTc5ODc2NTUzODU1ODcwOTY1/gettyimages-83414114.jpg",
-
+      setPeople(req.data)
     }
-  ])
+
+    fetchData();
+
+  } ,[])
+
+  console.log(people);
 
   const swiped=(direction,nameToDelete) =>{
     console.log('You swiped :'+nameToDelete);
@@ -47,7 +43,7 @@ function TinderCards() {
            onSwipe={(dir) => swiped(dir,person.name)}
            onCardLeftScreen={()=> outOffFrame(person.name)}
            >
-            <div style={{backgroundImage: `url(${person.url})`}}
+            <div style={{backgroundImage: `url(${person.imgUrl})`}}
             
             className="card">
               <h3>{person.name}</h3>
